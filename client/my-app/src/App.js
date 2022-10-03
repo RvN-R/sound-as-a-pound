fad2470a081b5de0714ef668c12d2a00429b5db2
Richard Van Nairn <rvannairn@googlemail.com>
2022-10-03 10:21:19 +0100

    Line Chart now pulls data from Mongo

--- client/my-app/src/App.js
+++ client/my-app/src/App.js
@@ -6,36 +6,67 @@ import LineChart from "./components/LineChart";
 
 function App() {
   const [mongoDbResponseData, setMongoDbResponseData] = useState([]);
-  const [graphData, setGraphData] = useState({
-    labels: GbpData.map((data) => data.date),
-    datasets: [
-      {
-        label: "Value of GBP against USD",
-        data: GbpData.map((data) => data.value),
-        backgroundColor: "red",
-      },
-    ],
-  });
+  const [GbpTemplate, setGbpTemplate] = useState(GbpData);
+  const [graphData, setGraphData] = useState(Chart);
+
+  function Chart() {
+    let date = [];
+    let value = [];
+
+    Axios.get("http://localhost:3001/")
+      .then((res) => {
+        for (const dataObj of res.data) {
+          date.push(dataObj.date);
+          value.push(dataObj.value);
+        }
+        setGraphData({
+          labels: date,
+          datasets: [
+            {
+              label: "Value of GBP against USD",
+              data: value,
+              backgroundColor: "red",
+            },
+          ],
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    const theChart = {
+      labels: GbpData.map((data) => data.date),
+      datasets: [
+        {
+          label: "Value of GBP against Euro",
+          data: GbpData.map((data) => data.value),
+          backgroundColor: "red",
+        },
+      ],
+    };
+    return theChart;
+  }
 
   useEffect(() => {
-    Axios.get("http://localhost:3001/").then((response) => {
-      console.log(response);
-      setMongoDbResponseData(response.data);
-    });
+    Chart();
   }, []);
+
+  // useEffect(() => {
+  //   Axios.get("http://localhost:3001/").then((response) => {
+  //     setMongoDbResponseData(response.data);
+  //   });
+  // }, []);
 
   return (
     <div className="App">
       <h1>List of Data from MongoDb</h1>
+
       {/* {mongoDbResponseData.map((val, key) => {
         return (
           <div>
             <ul>
               <li>£{val.value}</li>
-              <li>{val.day}</li>
-              <li>{val.month}</li>
-              <li>{val.time}</li>
-              <li>{val.year}</li>
+              <li>{val.date}</li>
             </ul>
           </div>
         );
